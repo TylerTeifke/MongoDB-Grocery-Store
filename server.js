ee1e8fb6b827c2a95eb40a586e42fc4201b7be59
TylerTeifke <tylerteifke@gmail.com>
2026-03-12 11:30:05 -0700

    Entered some data into the DB and managed to query it as well

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const connectDB = require('./config/dbConn')
+const Employee = require('./models/Employee')
 //forces the Windos DNS server to resolve
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
 //connect to MongoDB
 connectDB()
 
+const handleFind = async () => {
+    const query = Employee.findOne({ 'firstname': 'Jane' })
+    query.select('firstname lastname')
+    const Jane = await query.exec()
+    console.log('%s %s', Jane.firstname, Jane.lastname)
+}
+
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
+    handleFind()
 })
