b69e5f49142ff16a956a42698be89b4b83559919
TylerTeifke <tylerteifke@gmail.com>
2026-04-09 11:06:44 -0700

    Got the method for getting one customer to populate some of the fields.

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -8,7 +8,15 @@ const getAllCustomers = async (req, res) => {
 }
 
 const getOneCustomer = async (req, res) => {
-    const customer = await Customer.findOne({ firstname: req.params.first, lastname: req.params.last }).exec()
+    //Will find the specified customer, and will populate their cashier and product collections
+    //with the appropriate data
+    const customer = await Customer.findOne({ firstname: req.params.first, lastname: req.params.last })
+                                   .populate('employee')
+                                   .populate({
+                                    path: 'products',
+                                    populate: {path: 'details'}
+                                })
+
     if (!customer) {
         return res.status(409)
     }
